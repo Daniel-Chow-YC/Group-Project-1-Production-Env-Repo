577f1324abe95cd3463d0d8afccf2713980f7f9e
Daniel Chow <DChow@spartaglobal.com>
2020-01-24 14:30:30 +0000

    modified packer.json file

--- dev_env/recipes/default.rb
+++ dev_env/recipes/default.rb
@@ -30,7 +30,7 @@
 
 apt_update
 # package ["python3.6", "python3-pip"]
-package "python3-pip" do
+package "python-pip" do
   action :install
 end
 
@@ -38,26 +38,26 @@ end
 bash 'install dependecies for app' do
   user 'root'
   code <<-EOH
-  sudo pip3 install -U pluggy
-  sudo pip3 install -U atomicwrites==1.3.0
-  sudo pip3 install -U attrs==19.1.0
-  sudo pip3 install -U beautifulsoup4==4.8.0
-  sudo pip3 install -U certifi==2019.6.16
-  sudo pip3 install -U chardet==3.0.4
-  sudo pip3 install -U idna==2.8
-  sudo pip3 install -U importlib-metadata==0.19
-  sudo pip3 install -U more-itertools==7.2.0
-  sudo pip3 install -U packaging==19.1
-  sudo pip3 install -U py==1.8.0
-  sudo pip3 install -U pyparsing==2.4.2
-  sudo pip3 install -U pytest==5.1.0
-  sudo pip3 install -U requests==2.22.0
-  sudo pip3 install -U six==1.12.0
-  sudo pip3 install -U soupsieve==1.9.2
-  sudo pip3 install -U urllib3==1.25.3
-  sudo pip3 install -U wcwidth==0.1.7
-  sudo pip3 install -U zipp==0.5.2
-  sudo pip3 install configmanager
+  sudo pip install -U pluggy
+  sudo pip install -U atomicwrites==1.3.0
+  sudo pip install -U attrs==19.1.0
+  sudo pip install -U beautifulsoup4==4.8.0
+  sudo pip install -U certifi==2019.6.16
+  sudo pip install -U chardet==3.0.4
+  sudo pip install -U idna==2.8
+  sudo pip install -U importlib-metadata==0.19
+  sudo pip install -U more-itertools==7.2.0
+  sudo pip install -U packaging==19.1
+  sudo pip install -U py==1.8.0
+  sudo pip install -U pyparsing==2.4.2
+  sudo pip install -U pytest==5.1.0
+  sudo pip install -U requests==2.22.0
+  sudo pip install -U six==1.12.0
+  sudo pip install -U soupsieve==1.9.2
+  sudo pip install -U urllib3==1.25.3
+  sudo pip install -U wcwidth==0.1.7
+  sudo pip install -U zipp==0.5.2
+  sudo pip install configmanager
   EOH
 end
 
